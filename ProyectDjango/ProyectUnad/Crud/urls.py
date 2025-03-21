95a48a163cc69da8edbb04cae4069e10f33b2d7f
Andrekcar <126022147+Andrekcar@users.noreply.github.com>
2025-03-21 12:54:49 +0000

    - Se añade un ejemplo basico de interaccion entre pestañas - Se elimina temporalmente el archivo .gitignore para evitar conflictos actuales - Se añade un archivo de plantilla para la pestaña de seccion uno

--- ProyectDjango/ProyectUnad/Crud/urls.py
+++ ProyectDjango/ProyectUnad/Crud/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Crud, name="index"),
+    path('' , views.Crud, name='index'),
+    path('sectionOne.html/' , views.sectionOne, name='sectionOne'),
+    
 ]
--- ProyectDjango/ProyectUnad/Crud/views.py
+++ ProyectDjango/ProyectUnad/Crud/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def Crud(request):
     return render(request, 'index.html')
+
+def sectionOne(request):
+    return render(request, 'sectionOne.html')
