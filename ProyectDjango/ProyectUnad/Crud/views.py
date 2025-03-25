9b3b43b64832b0c8a07e5455bc3e3dbb13fa0981
FabioReal85 <fabiospia@gmail.com>
2025-03-24 20:27:00 -0500

    Agregar update en index y view

--- ProyectDjango/ProyectUnad/Crud/views.py
+++ ProyectDjango/ProyectUnad/Crud/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import producto, categoria
 
 def Crud(request):
     return render(request, 'index.html')
 
-#crud
+def actualizar_producto(request):
+    if request.method == "POST":
+        producto_id = request.POST.get('producto_id')
+        nombre = request.POST.get('nombre')
+        descripcion = request.POST.get('descripcion')
+        precio = request.POST.get('precio')
 
-def Tienda(request):
-    categorias = categoria.objects.all()
-    productos = producto.objects.all()
+        # Buscar el producto y actualizarlo
+        producto_obj = get_object_or_404(producto, id=producto_id)
+        producto_obj.nombre = nombre
+        producto_obj.descripcion = descripcion
+        producto_obj.precio = precio
+        producto_obj.save()
 
-    return render(request, 'index.html', {'categorias': categorias, 'productos': productos})
+        return redirect('index')  # Asegúrate de que esta URL está definida en urls.py
 
+    return redirect('index')  # Si no es POST, regresa al CRUD
 
 
 #CREAR - CAMILO
